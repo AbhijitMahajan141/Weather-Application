0370e368445d565a61add185aafe386d05f0d564
AbhijitMahajan141 <abhimahajan142001@gmail.com>
2022-09-10 21:55:14 +0530

    First Commit

---
+++ vatavaran/src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a4ebfd7f10mshaf6a6e7f12ad007p10e4f5jsn8fef7c5f7db7",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "7c6bdc17c10d5cd72c9b77bbba74997a";
