2f876cfd35e957937322a17db02afb031d10ea2e
Abhijit Mahajan <82993630+AbhijitMahajan141@users.noreply.github.com>
2022-09-10 22:09:56 +0530

    Update api.js

--- vatavaran/src/api.js
+++ vatavaran/src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "a4ebfd7f10mshaf6a6e7f12ad007p10e4f5jsn8fef7c5f7db7",
+    "X-RapidAPI-Key": "YOUR rapidAPI API key HERE",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "7c6bdc17c10d5cd72c9b77bbba74997a";
+export const WEATHER_API_KEY = "Your OpenWeather API key HERE";
